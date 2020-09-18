72399726a9c89dff5319c1b6aab137eaea5c5063
Saurabh Shinde <saurabhshinde391@gmail.com>
2020-09-18 16:49:32 +0530

    Added stories for different designs

--- src/app/service.tsx
+++ src/app/service.tsx
@@ -1,17 +1,7 @@
 import { isElectron, ipcRendererFunc } from '../utils/isElectron';
-import HttpStatus from 'http-status-codes';
 import RpcClient from '../utils/rpc-client';
-import showNotification from '../utils/notifications';
 import * as log from '../utils/electronLogger';
-import { I18n } from 'react-redux-i18n';
-// import { isBlockchainStarted } from '../containers/RpcConfiguration/service';
 import { eventChannel } from 'redux-saga';
-// import {
-//   fetchWalletBalanceRequest,
-//   fetchPendingBalanceRequest,
-// } from '../containers/WalletPage/reducer';
-import store from '../app/rootStore';
-import { DUMP_WALLET, IMPORT_WALLET } from '../constants/rpcMethods';
 
 export const getRpcConfig = () => {
   if (isElectron()) {
@@ -50,28 +40,10 @@ export const stopBinary = () => {
 
 export const backupWallet = async (paths: string) => {
   const rpcClient = new RpcClient();
-  const res = await rpcClient.call('', DUMP_WALLET, [paths]);
-
-  if (res.status === HttpStatus.OK) {
-    return showNotification(
-      I18n.t('alerts.success'),
-      I18n.t('alerts.backupSuccess')
-    );
-  }
-  return showNotification(I18n.t('alerts.errorOccurred'), res.data.error);
+  return '';
 };
 
 export const importWallet = async (paths: string[]) => {
   const rpcClient = new RpcClient();
-  const res = await rpcClient.call('', IMPORT_WALLET, paths);
-  if (res.status === HttpStatus.OK) {
-    // store.dispatch(fetchWalletBalanceRequest()); // Check for new Balance;
-    // store.dispatch(fetchPendingBalanceRequest()); // Check for new Pending Balance;
-
-    return showNotification(
-      I18n.t('alerts.success'),
-      I18n.t('alerts.importSuccess')
-    );
-  }
-  return showNotification(I18n.t('alerts.errorOccurred'), res.data.error);
+  return '';
 };
