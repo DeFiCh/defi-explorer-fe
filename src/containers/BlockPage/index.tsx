16541f48e867fcde1203454cd12f3e69ef4e0ad8
Harsh Rathi <fullstackninja864@gmail.com>
2020-10-13 18:04:14 +0530

    Fix linting

--- src/containers/BlockPage/index.tsx
+++ src/containers/BlockPage/index.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect } from "react";
-import CopyToClipIcon from "../../components/CopyToClipIcon";
-import { connect } from "react-redux";
-import { I18n } from "react-redux-i18n";
-import { RouteComponentProps } from "react-router-dom";
-import { Col, Row } from "reactstrap";
-import KeyValueLi from "../../components/KeyValueLi";
-import { AppBlock } from "../../utils/interfaces";
-import { getBlockFromHash } from "./reducer";
-import { BLOCK_PAGE_BASE_PATH } from "../../constants";
-import BlockTxn from "../../components/BlockTxn";
-import moment from "moment";
+import React, { useEffect } from 'react';
+import CopyToClipIcon from '../../components/CopyToClipIcon';
+import { connect } from 'react-redux';
+import { I18n } from 'react-redux-i18n';
+import { RouteComponentProps } from 'react-router-dom';
+import { Col, Row } from 'reactstrap';
+import KeyValueLi from '../../components/KeyValueLi';
+import { AppBlock } from '../../utils/interfaces';
+import { getBlockFromHash } from './reducer';
+import { BLOCK_PAGE_BASE_PATH } from '../../constants';
+import BlockTxn from '../../components/BlockTxn';
+import moment from 'moment';
 
 interface RouteInfo {
   blockHash: string;
@@ -50,8 +50,6 @@ const BlockPage: React.FunctionComponent<BlockPageProps> = (
     transactions,
   } = props;
 
-  console.log(time);
-
   useEffect(() => {
     getBlockFromHash(params.blockHash);
   }, [params]);
@@ -61,86 +59,86 @@ const BlockPage: React.FunctionComponent<BlockPageProps> = (
     if (isError) return <div>{isError}</div>;
     return (
       <Row>
-        <Col xs="12">
+        <Col xs='12'>
           <h1>
-            {I18n.t("containers.blockPage.blockTitle", {
+            {I18n.t('containers.blockPage.blockTitle', {
               height,
             })}
           </h1>
-          <div className="my-2">
-            <span>{I18n.t("containers.blockPage.blockHash")}: </span>
+          <div className='my-2'>
+            <span>{I18n.t('containers.blockPage.blockHash')}: </span>
             <span>{hash}</span>
             <span>
-              <CopyToClipIcon value={hash!} uid={"blockHashCopy"} />
+              <CopyToClipIcon value={hash!} uid={'blockHashCopy'} />
             </span>
           </div>
         </Col>
-        <Col xs="12">
-          <h1>{I18n.t("containers.blockPage.summary")}</h1>
+        <Col xs='12'>
+          <h1>{I18n.t('containers.blockPage.summary')}</h1>
           <Row>
-            <Col xs="12" md="6">
+            <Col xs='12' md='6'>
               <KeyValueLi
-                label={I18n.t("containers.blockPage.numOfTxns")}
+                label={I18n.t('containers.blockPage.numOfTxns')}
                 value={`${txlength}`}
               />
               <KeyValueLi
-                label={I18n.t("containers.blockPage.height")}
+                label={I18n.t('containers.blockPage.height')}
                 value={`${height}`}
               />
               <KeyValueLi
-                label={I18n.t("containers.blockPage.block")}
+                label={I18n.t('containers.blockPage.block')}
                 value={`${reward}`}
               />
               <KeyValueLi
-                label={I18n.t("containers.blockPage.timestamp")}
-                value={`${moment(time).format("lll")}`}
+                label={I18n.t('containers.blockPage.timestamp')}
+                value={`${moment(time).format('lll')}`}
               />
               <KeyValueLi
-                label={I18n.t("containers.blockPage.merkleRoot")}
+                label={I18n.t('containers.blockPage.merkleRoot')}
                 value={`${merkleroot}`}
               />
               <KeyValueLi
-                label={I18n.t("containers.blockPage.previousBlock")}
+                label={I18n.t('containers.blockPage.previousBlock')}
                 value={`${height - 1}`}
                 href={`${BLOCK_PAGE_BASE_PATH}/${previousblockhash}`}
               />
             </Col>
-            <Col xs="12" md="6">
+            <Col xs='12' md='6'>
               <KeyValueLi
-                label={I18n.t("containers.blockPage.difficulty")}
+                label={I18n.t('containers.blockPage.difficulty')}
                 value={`${difficulty}`}
               />
               <KeyValueLi
-                label={I18n.t("containers.blockPage.bits")}
+                label={I18n.t('containers.blockPage.bits')}
                 value={`${bits}`}
               />
               <KeyValueLi
-                label={I18n.t("containers.blockPage.size")}
+                label={I18n.t('containers.blockPage.size')}
                 value={`${size}`}
               />
               <KeyValueLi
-                label={I18n.t("containers.blockPage.version")}
+                label={I18n.t('containers.blockPage.version')}
                 value={`${version}`}
               />
               <KeyValueLi
-                label={I18n.t("containers.blockPage.nextBlock")}
+                label={I18n.t('containers.blockPage.nextBlock')}
                 value={`${height + 1}`}
                 href={
                   !!nextblockhash && `${BLOCK_PAGE_BASE_PATH}/${nextblockhash}`
                 }
               />
               <KeyValueLi
-                label={I18n.t("containers.blockPage.confirmations")}
+                label={I18n.t('containers.blockPage.confirmations')}
                 value={`${confirmations}`}
               />
             </Col>
           </Row>
         </Col>
-        <Col xs="12">{/* <BlockTxn /> */}</Col>
+        <Col xs='12'>{/* <BlockTxn /> */}</Col>
       </Row>
     );
   };
-  return <div className="mt-4">{loadHtml()}</div>;
+  return <div className='mt-4'>{loadHtml()}</div>;
 };
 
 const mapStateToProps = (state) => {
