70bfc3472986a89c6c8591dfc8a3a2e00de4c80e
monstrobishi <monstrobishi@gmail.com>
2021-05-15 15:39:39 +0800

    Added link back to explorer for transaction

--- src/containers/TxInfoPage/index.tsx
+++ src/containers/TxInfoPage/index.tsx
@@ -3,6 +3,13 @@ import { connect } from 'react-redux';
 import { I18n } from 'react-redux-i18n';
 import { fetchTxInfoStartedRequest } from './reducer';
 import styles from './TxInfoPage.module.scss';
+import {
+  DEFAULT_DECIMAL_PLACE,
+  POOL_LIST_PAGE_URL_NAME,
+  TOKENS_LIST_PAGE_LIMIT,
+  MAINNET_EXPLORER,
+  TESTNET_EXPLORER,
+} from '../../constants';
 
 const txInfoPage = (props) => {
   const {
@@ -19,9 +26,17 @@ const txInfoPage = (props) => {
     fetchTxInfoStartedRequest(txid);
   }, [txid]);
 
+  const API_PREFIX =
+    props.network === 'mainnet' ? MAINNET_EXPLORER : TESTNET_EXPLORER;
+
   return (
     <>
       <h1>{I18n.t('containers.txInfoPage.txInfoPageTitle')}</h1>
+      <h3>
+        Transaction ID:&nbsp;
+        <a href={`${API_PREFIX}tx/${txid}`}>{txid}</a>
+      </h3>
+
       <p className={styles.infoText}>
         {typeof props.data === 'object'
           ? JSON.stringify(props.data, null, 4)
@@ -33,6 +48,7 @@ const txInfoPage = (props) => {
 
 const mapStateToProps = ({ txInfoPage, app }) => ({
   data: txInfoPage.data,
+  network: app.network,
 });
 
 const mapDispatchToProps = {
